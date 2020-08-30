d83789c2e0674fff15d96dbd2b4b5d0d53b3438b
thethewerd <70454096+thethewerd@users.noreply.github.com>
2020-08-30 15:43:47 -0400

    funckin for quiz (#2)

---
+++ functions.py
@@ -0,0 +1,9 @@
+def get_user_input_int(questin, message, min, ):
+    questin_nomber = input (message)
+    user_anser = input (questin)
+    if user_anser <= max and user_anser >= min:
+    else:
+        
+        print ("you can't do this nomber")
+def get_user_input_string(message, allowableList):
+    questin_operatin = input (message)
